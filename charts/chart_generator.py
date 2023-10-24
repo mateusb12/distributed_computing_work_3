6a73d05ba67fa83304b38fd16706d8d1e898fe39
mateusb12 <mateus_b09@hotmail.com>
2023-10-24 16:07:37 -0300

    Add aggregate function to improve chart accuracy
    An aggregate function, set to median, was added to 'create_chart' function to enhance the accuracy of charts. The labels on 'response_time_chart' and 'requisitions_per_second_chart' were also adjusted to more accurately represent the data being visualized. The changes do not affect the core functionality but enhance the data computation and representation for better usability.

--- charts/chart_generator.py
+++ charts/chart_generator.py
@@ -4,16 +4,17 @@ import numpy as np
 
 CSV_FILE = ("C:\\Users\\Mateus\\Desktop\\unifor\\computacao_distribuida\\docker_unifor\\final_results\\"
             "csvs\\merged_history.csv")
+AGGREGATE_FUNCTION = 'median'
 
 
 def create_chart(input_df, y_value_column, y_label, title, colors):
     pivot_table = input_df[input_df['User Count'].isin([10, 100, 1000])].pivot_table(
         index='User Count', columns='Containers',
-        values=y_value_column).reset_index()
+        values=y_value_column, aggfunc=AGGREGATE_FUNCTION).reset_index()
 
     labels = pivot_table['User Count'].tolist()
-    width = 0.20  # width of the bars
-    x = np.arange(len(labels))  # the label locations
+    width = 0.20
+    x = np.arange(len(labels))
 
     fig, ax = plt.subplots(figsize=(10, 6))
 
@@ -33,13 +34,13 @@ def create_chart(input_df, y_value_column, y_label, title, colors):
 
 
 def response_time_chart(input_df):
-    colors = ['#cfe2f3', '#fce5cd', '#f4cccc']  # blue, yellow, red
+    colors = ['#cfe2f3', '#fce5cd', '#f4cccc']
     create_chart(input_df, 'Total Average Response Time', 'Tempo de resposta (ms)',
-                 'Tempo médio total de resposta vs número de usuários', colors)
+                 'Tempo médio de resposta vs número de usuários', colors)
 
 
 def requisitions_per_second_chart(input_df):
-    colors = ['#d9ead3', '#d0e0e3', '#fff2cc']  # green, blue, yellow
+    colors = ['#d9ead3', '#d0e0e3', '#fff2cc']
     create_chart(input_df, 'Requests/s', 'Requisições por segundo',
                  'Requisições por segundo vs número de usuários', colors)
 
